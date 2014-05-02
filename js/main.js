72ddde9253bd3f2529d5622daa794c7fa2b481e6
david <davidcp90@gmail.com>
2014-05-02 10:59:57 -0500

    Finished

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
 $( window ).load(function() {
-$('#start').click(function(e) {
-    e.preventDefault();
-    $('#intro').fadeOut('fast');
-    $('#questions').fadeIn('fast');
-    resizeContainers();
-    $('#questions').transition({ opacity: 1, delay: 100 });
-    chrono();
-});
+    $('#start').click(function(e) {
+        e.preventDefault();
+        $('#intro').fadeOut('fast');
+        $('#questions').fadeIn('fast');
+        resizeContainers();
+        $('#questions').transition({ opacity: 1, delay: 100 });
+        chrono();
+    });
+    $('#trig-one').click(function(e){
+        choice=$(this).attr('data-choice');
 
+        if(choice=="1"){
 
+            sumScore();
+        }
+        else{
+            cheer=false;
+            cheerUp(cheer);
+        }
+    });
+    $('#trig-two').click(function(e){
+        choice=$(this).attr('data-choice');
+
+        if(choice=="1"){
+
+            sumScore();
+        }
+        else{
+            cheer=false;
+            cheerUp(cheer);
+        }
+    });
 });
+var jsonp ='[{ "pregunta": "There more that 5 Latinamerican countries in the bottom quarter of the PISA results?", "opt_one": "True", "opt_two": "False", "answer": "choice-one"}, {"pregunta": "Proficiency in mathematics is a strong predictor of positive outcomes for young adults, influencing in their expected future earnings", "opt_one": "False", "opt_two": "True", "answer": "choice-two"}, {"pregunta": "Out of 65 countries in the PISA study, Colombia ranked 64 in math", "opt_one": "False", "opt_two": "True", "answer": "choice-two"} ]';
+var ans_count=1,
+score=0;
+read_obj=JSON.parse(jsonp);
 function resizeContainers(){
     h=$(window).height();
     ha=h*0.5;
@@ -17,26 +43,81 @@ function resizeContainers(){
     $('.two').height(ha);
     $('.full').height(hd);
 }
+var epiphany;
 function chrono(){
     var start = new Date;
-
-    setInterval(function() {
+    epiphany=setInterval(function() {
         value_time=parseInt(new Date - start);
         value_time=value_time/1000;
         value_time=Math.floor(value_time);
         if(value_time<=30){
             $('#time').html(value_time);
-
+            $('.progress').transition({ rotate: '+=30deg'});
+        }
+        else{
+            clearInterval(epiphany);
+            loadNew();
         }
     }, 1000);
-    setInterval(function() {
-        value_time_g=parseInt(new Date - start);
-        value_time_g=value_time_g/5;
-        value_time_g=Math.floor(value_time_g);
-        if(value_time_g<=30){
 
-            $('.progress').transition({ rotate: '+=15deg'});
-        }
-    }, 5);
+
 
 }
+
+function loadNew(){
+    ans_count++;
+    clearInterval(epiphany);
+    if(ans_count<5){
+        $('#qnum').html(ans_count);
+        re=read_obj[ans_count-2];
+        $('#question').html(re.pregunta);
+        $('#chu').html(re.opt_one);
+        $('#chd').html(re.opt_two);
+        if(re.answer=="choice-one"){
+            $('#trig-one').attr('data-choice','1');
+            $('#trig-two').attr('data-choice','0');
+        }
+        else{
+            $('#trig-one').attr('data-choice','0');
+            $('#trig-two').attr('data-choice','1');
+        }
+        chrono();
+    }
+        else{
+            showScore();
+        }
+
+
+    }
+    function sumScore(){
+        var time_sc=parseInt($('#time').html());
+        time_sc=(30-time_sc)+100;
+        score+=time_sc;
+        cheer=true;
+        cheerUp(cheer);
+
+    }
+    function showScore(){
+        $('.question-top').fadeOut('fast', function() {
+            $('#badge-container').fadeIn('fast').transition({ opacity: 1, delay: 100 });
+            $('#choice-container').fadeOut('fast');
+            $('#scoredisplay').fadeIn('fast').transition({ opacity: 1, delay: 100 });
+            $('#uscore').html(score);
+        });
+    }
+    function cheerUp(cheer){
+        if(cheer){
+            $('#modalyeah').fadeIn('fast',function(){
+                $(this).delay(1000).fadeOut('fast', function() {
+                    loadNew();
+                });
+            });
+        }
+        else{
+            $('#modalwrong').fadeIn('fast',function(){
+                $(this).delay(1000).fadeOut('fast', function() {
+                    loadNew();
+                });
+            });
+        }
+    }
